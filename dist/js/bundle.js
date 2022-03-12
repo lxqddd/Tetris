3e74d0962437824b23e80b3fda17ca30128e28a6
lxq <xiangqian_liu@163.com>
2022-03-12 17:34:28 +0800

    feat: 项目初始化

---
+++ dist/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+var msg = 'hello world';
+console.log(msg);
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    hot: true
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader']
+      }
+    ]
+  }
+}
