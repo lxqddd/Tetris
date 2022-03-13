6845083ed7882d7798872b17c5672c293eade2b9
lxq <xiangqian_liu@163.com>
2022-03-13 23:46:19 +0800

    docs: 添加一些注释

--- src/core/Square/SquareGroup.ts
+++ src/core/Square/SquareGroup.ts
@@ -1,5 +1,8 @@
 import { IPoint, Shape } from '../types'
 import Square from './Square'
+/**
+ * 方块组合类
+ */
 export default class SquareGroup {
   private _squares: readonly Square[]
 
@@ -23,5 +26,22 @@ export default class SquareGroup {
 
   public get squares(): readonly Square[] {
     return this._squares
+  }
+
+  public get centerPoint(): IPoint {
+    return this._centerPoint
+  }
+
+  /**
+   * 更新中心点位置的时候更新每个小方块的坐标
+   */
+  public set centerPoint(v: IPoint) {
+    this._centerPoint = v
+    this._shape.forEach((sq, index) => {
+      this._squares[index].point = {
+        x: this._centerPoint.x + sq.x,
+        y: this._centerPoint.y + sq.y
+      }
+    })
   }
 }
--- src/core/types.ts
+++ src/core/types.ts
@@ -1,17 +1,50 @@
+/**
+ * 方块坐标接口
+ */
 export interface IPoint {
+  /**
+   * 横坐标
+   */
   readonly x: number
+  /**
+   * 纵坐标
+   */
   readonly y: number
 }
 
+/**
+ * 显示者接口
+ */
 export interface IViewer {
+  /**
+   * 控制显示
+   */
   show(): void
+  /**
+   * 控制删除
+   */
   remove(): void
 }
 
+/**
+ * 小方块显示接口
+ */
 export interface ISquare {
+  /**
+   * 小方块宽度
+   */
   width: number
+  /**
+   * 小方块高度
+   */
   height: number
+  /**
+   * 小方块边框
+   */
   border: string
 }
 
+/**
+ * 形状
+ */
 export type Shape = IPoint[]
